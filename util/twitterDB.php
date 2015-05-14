91ce275f8bf20513dab595ef5116196f4761e5dc
Brian Blackwell <industriousthought@yahoo.com>
2015-05-14 20:06:29 +0000

    added db connect function

--- util/twitterDB.php
+++ util/twitterDB.php
@@ -1,5 +1,10 @@
 <?php 
 
+$mysqli = new mysqli("server", "user", "password", "database");
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+}
+
 $sql = "CREATE TABLE twitter (
 
 	rId INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
