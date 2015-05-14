9ee2321a2252b2d711ecbdf4fd6f77a1567b9c34
Brian Blackwell <industriousthought@yahoo.com>
2015-05-14 19:54:07 +0000

    minor changes

--- schema.php
+++ schema.php
@@ -25,7 +25,7 @@ $exchanges = array(
 );
 
 
-$mysqli = new mysqli("localhost", "bitcoin", "btcPW$", "bitcoin");
+$mysqli = new mysqli("server", "user", "password", "database");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: " . $mysqli->connect_error;
 }
